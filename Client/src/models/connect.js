835bbe41797dd58e8be21d7caa4b88ee0669f20d
Vu Luu Duc <vuluuduc2001@gmai.com>
2021-11-29 21:20:41 +0700

    fix shop-left-sidebar to shop

--- Client/src/models/connect.js
+++ Client/src/models/connect.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  database: 'laptop_webshop',
+  host: 'sql6.freemysqlhosting.net',
+  user: 'sql6454865',
+  password: '7zB5KXjdxg',
+  database: 'sql6454865',
   charset: 'utf8_general_ci',
   port: '3306'
 });
