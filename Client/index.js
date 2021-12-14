5fda4bf45ba46574032cda1e4fa903777660adb6
tminhbao <tminhbao2001@gmail.com>
2021-12-14 14:41:16 +0700

    Fix host database (Client)

--- Client/index.js
+++ Client/index.js
@@ -1,39 +1,40 @@
-const express = require('express')
-const handlebars = require('express-handlebars');
-const hbs = handlebars.create({ extname: '.hbs' })
-const app = express()
-const port = 3000
-const route = require('./src/routes/index');
-const bodyParser = require('body-parser');
+const express = require("express");
+const handlebars = require("express-handlebars");
+const hbs = handlebars.create({ extname: ".hbs" });
+const app = express();
+const port = 3000;
+const route = require("./src/routes/index");
+const bodyParser = require("body-parser");
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-const path = require('path');
-app.set("views", path.join(__dirname, 'src/resources/views'));
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+const path = require("path");
+app.set("views", path.join(__dirname, "src/resources/views"));
 
-app.use(express.static('src/public'));
+app.use(express.static("src/public"));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const connectFlash = require("connect-flash");
+const passport = require("passport");
 
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const connectFlash = require('connect-flash');
-const passport = require('passport')
+app.use(cookieParser("secret"));
 
-app.use(cookieParser('secret'));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
-app.use(session({
-  secret:'secret',
-  resave:false,
-  saveUninitialized:true,
-  cookie: {
-    maxAge:1000*60*60*24
-  }
-}));
-
-// Enable flash message 
+// Enable flash message
 app.use(connectFlash());
 
 app.use(passport.initialize());
@@ -42,5 +43,5 @@ app.use(passport.session());
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- Client/src/configs/connect.js
+++ Client/src/configs/connect.js
@@ -1,18 +1,15 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 const conn = mysql.createConnection({
-  host: 'sql6.freemysqlhosting.net',
-  user: 'sql6456787',
-  password: 'VRPL3AsQP4',
-  database: 'sql6456787',
-  charset: 'utf8_general_ci',
-  port: '3306',
-  multipleStatements:true
+  host: "db4free.net",
+  user: "minhbao",
+  password: "123456789",
+  database: "laptop_webshop",
+  charset: "utf8_general_ci",
+  port: "3306",
+  multipleStatements: true,
 });
 conn.connect(function (err) {
-  if (err)
-    throw err;
-  console.log('Kết nối database thành công!!!');
-})
+  if (err) throw err;
+  console.log("Kết nối database thành công!!!");
+});
 module.exports = conn;
-
-
